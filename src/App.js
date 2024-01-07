a1adeea6f7c37f4690f81c35a0ccbac28ef89ca7
srikanthganta182 <srikanth.ganta@ixor.be>
2024-01-07 09:26:39 +0530

    Update link text in App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Now!
+          Auto Deploy works!! :D 
         </a>
       </header>
     </div>
